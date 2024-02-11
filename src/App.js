9af18aeb94b234b724098692e051a72f282e5f59
JAMES KABOGO NJOROGE <kabogp@gmail.com>
2024-02-11 23:51:39 +0300

    added a form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BlogForm from './components/BlogForm';
+// import YourExistingBlogComponent from './components/YourExistingBlog';
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
+      <BlogForm />
+      {/* <YourExistingBlogComponent/> */}
       <div className='content'>
 
       </div>
